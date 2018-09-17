9f8687b2d98bb8a8edfda21bdb710aab566af0e2
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-09-17 12:37:56 +0100

    MISC : Fixes for key.py.

--- hitch/key.py
+++ hitch/key.py
@@ -1,38 +1,16 @@
-from commandlib import Command, python
+import hitchpylibrarytoolkit
 from hitchrun import DIR
 
 
 def deploy(version):
     """
-    Deploy to pypi as specified version.
+    Deploy specified version to pypi.
     """
-    NAME = "hitchpylibrarytoolkit"
-    git = Command("git").in_dir(DIR.project)
-    version_file = DIR.project.joinpath("VERSION")
-    old_version = version_file.bytes().decode('utf8')
-    if version_file.bytes().decode("utf8") != version:
-        DIR.project.joinpath("VERSION").write_text(version)
-        git("add", "VERSION").run()
-        git("commit", "-m", "RELEASE: Version {0} -> {1}".format(
-            old_version,
-            version
-        )).run()
-        git("push").run()
-        git("tag", "-a", version, "-m", "Version {0}".format(version)).run()
-        git("push", "origin", version).run()
-    else:
-        git("push").run()
+    hitchpylibrarytoolkit.deploy(DIR.project, "hitchpylibrarytoolkit", version)
 
-    # Set __version__ variable in __init__.py, build sdist and put it back
-    initpy = DIR.project.joinpath(NAME, "__init__.py")
-    original_initpy_contents = initpy.bytes().decode('utf8')
-    initpy.write_text(
-        original_initpy_contents.replace("DEVELOPMENT_VERSION", version)
-    )
-    python("setup.py", "sdist").in_dir(DIR.project).run()
-    initpy.write_text(original_initpy_contents)
 
-    # Upload to pypi
-    python(
-        "-m", "twine", "upload", "dist/{0}-{1}.tar.gz".format(NAME, version)
-    ).in_dir(DIR.project).run()
+def reformat():
+    """
+    Reformat using black and then relint.
+    """
+    hitchpylibrarytoolkit.reformat(DIR.project, "hitchpylibrarytoolkit")
