47c70b11e12297973dca5bbe28b7a9b2a1444ad0
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-09-17 12:39:13 +0100

    BUGFIX : Fix importing.

--- hitchpylibrarytoolkit/__init__.py
+++ hitchpylibrarytoolkit/__init__.py
@@ -1,6 +1,6 @@
-from deploy import deploy
-from docgen import docgen
-from build import project_build
-from formatter import reformat
+from hitchpylibarytoolkit.deploy import deploy
+from hitchpylibarytoolkit.docgen import docgen
+from hitchpylibarytoolkit.build import project_build
+from hitchpylibarytoolkit.formatter import reformat
 
 __version__ = "0.1.0"
