141e765db3de44c61f99f5e1d94f2fb7b2476f98
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-09-17 13:37:01 +0100

    BUGFIX : Split lint and black.

--- hitchpylibrarytoolkit/__init__.py
+++ hitchpylibrarytoolkit/__init__.py
@@ -2,5 +2,7 @@ from hitchpylibrarytoolkit.deploy import deploy
 from hitchpylibrarytoolkit.docgen import docgen
 from hitchpylibrarytoolkit.build import project_build
 from hitchpylibrarytoolkit.formatter import reformat
+from hitchpylibrarytoolkit.formatter import lint
+
 
 __version__ = "0.1.0"
--- hitchpylibrarytoolkit/formatter.py
+++ hitchpylibrarytoolkit/formatter.py
@@ -1,9 +1,7 @@
 from commandlib import python, python_bin
 
 
-def reformat(project_dir, project_name):
-    python_bin.black(project_dir / project_name).run()
-    python_bin.black(project_dir / "hitch" / "key.py").run()
+def lint(project_dir, project_name):
     python("-m", "flake8")(
         project_dir.joinpath(project_name),
         "--max-line-length=100",
@@ -14,7 +12,10 @@ def reformat(project_dir, project_name):
         "--max-line-length=100",
         "--exclude=__init__.py",
     ).run()
-    print("Reformat / lint success")
 
 
-3
+def reformat(project_dir, project_name):
+    python_bin.black(project_dir / project_name).run()
+    python_bin.black(project_dir / "hitch" / "key.py").run()
+    print("Reformat success")
+    lint(project_dir, project_name)
