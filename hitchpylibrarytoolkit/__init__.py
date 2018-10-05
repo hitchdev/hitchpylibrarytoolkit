894ab86a526900b711c0e8af94458f05367a52db
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-10-05 18:45:22 +0100

    FEATURE : Readme generation.

--- hitchpylibrarytoolkit/__init__.py
+++ hitchpylibrarytoolkit/__init__.py
@@ -1,5 +1,6 @@
 from hitchpylibrarytoolkit.deploy import deploy
 from hitchpylibrarytoolkit.docgen import docgen
+from hitchpylibrarytoolkit.docgen import readmegen
 from hitchpylibrarytoolkit.build import project_build
 from hitchpylibrarytoolkit.formatter import reformat
 from hitchpylibrarytoolkit.formatter import lint
--- hitchpylibrarytoolkit/docgen.py
+++ hitchpylibrarytoolkit/docgen.py
@@ -2,16 +2,17 @@ import dirtemplate
 from strictyaml import load
 
 
+def title(dirfile):
+    assert len(dirfile.text().split("---")) >= 3, "{} doesn't have ---".format(
+        dirfile
+    )
+    return load(dirfile.text().split("---")[1]).data.get("title", "misc")
+
+
 def docgen(all_stories, project_dir, story_dir, build_dir):
     """
     Generate markdown documentation.
     """
-
-    def title(dirfile):
-        assert len(dirfile.text().split("---")) >= 3, "{} doesn't have ---".format(
-            dirfile
-        )
-        return load(dirfile.text().split("---")[1]).data.get("title", "misc")
 
     docfolder = build_dir / "docs"
     if docfolder.exists():
@@ -37,3 +38,38 @@ def docgen(all_stories, project_dir, story_dir, build_dir):
     )
     template.ensure_built()
     print("Docs generated")
+
+
+def readmegen(all_stories, project_dir, story_dir, build_dir, project_name):
+    docfolder = build_dir / "readme"
+    if docfolder.exists():
+        docfolder.rmtree(ignore_errors=True)
+    docfolder.mkdir()
+
+    template = (
+        dirtemplate.DirTemplate("readme", project_dir / "docs", build_dir)
+        .with_files(
+            template_story_jinja2={}
+        )
+        .with_vars(
+            readme=True,
+            quickstart=all_stories.in_filename(story_dir / "quickstart.story")
+            .non_variations()
+            .ordered_by_file(),
+        )
+        .with_functions(title=title)
+    )
+    template.ensure_built()
+
+    readme_text = docfolder.joinpath("index.md").text()
+
+    import re
+
+    text_with_absolute_links = re.sub(
+        r"(\[.*?\])\(((?!http).*?)\)",
+        "\g<1>(https://hitchdev.com/{0}/\g<2>)".format(project_name),
+        readme_text
+    )
+
+    project_dir.joinpath("README.md").write_text(text_with_absolute_links)
+    print("README generated")
