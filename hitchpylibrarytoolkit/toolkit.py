ce2b59344ccfb228fe099d0cbfeb549ce9b11ef6
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-12-02 16:28:50 +0000

    FEATURE : Created toolkit class to derive all common operations from.

---
+++ hitchpylibrarytoolkit/toolkit.py
@@ -0,0 +1,50 @@
+from hitchpylibrarytoolkit.deploy import deploy
+from hitchpylibrarytoolkit.docgen import docgen
+from hitchpylibrarytoolkit.docgen import readmegen
+from hitchpylibrarytoolkit.build import project_build
+from hitchpylibrarytoolkit.formatter import reformat
+from hitchpylibrarytoolkit.formatter import lint
+from hitchstory import StoryCollection
+from pathquery import pathquery
+
+
+class ToolkitError(Exception):
+    pass
+
+
+class ProjectToolkit(object):
+    def __init__(self, project_name, paths, engine_class):
+        self._project_name = project_name
+        self._path = paths
+        self._engine_class = engine_class
+
+    @property
+    def current_version(self):
+        return self._path.project.joinpath("VERSION").text().rstrip()
+
+    def bdd(self, *keywords):
+        self._stories().only_uninherited().shortcut(*keywords).play()
+
+    def _stories(self, **engineargs):
+        return StoryCollection(
+            pathquery(self._path.key / "story").ext("story"), self._engine_class(self._path, **engineargs)
+        )
+
+    def prepdeploy(self):
+        self.regression()
+        readmegen(self._stories(), self._path.project, self._path.key, self._path.gen), self._project_name)
+
+    def deploy(self, version):
+        deploy(self._path.project, self._project_name, version)
+
+    def lint(self):
+        lint(self._path.project, self._project_name)
+
+    def reformat(self):
+        reformat(self._path.project, self._project_name)
+
+    def docgen(self):
+        docgen(self._stories(), self._path.project, self._path.key, self._path.gen)
+
+    def project_build(self, python_version="3.7.0", libraries=None):
+        project_build(self._project_name, self._path, python_version, libraries=libraries)
